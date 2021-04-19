88a2fbac74ae2b8d51734b846e31d73de33911f7
Peter <dhondt.peter@gmail.com>
2021-04-19 12:06:07 +0200

    configured client-server connection

--- server/server.js
+++ server/server.js
@@ -2,15 +2,22 @@ const express = require('express');
 const http = require('http');
 
 const app = express();
-const clientPath = '${__dirname}/../client';
+const clientPath = `${__dirname}/../client`;
 
 app.use(express.static(clientPath));
 const server = http.createServer(app);
 
 const io = require('socket.io')(server);
 
-const port = 8080;
+const port = 3000;
+
+let counter = 0;
 
 server.listen(port, () => {
-    console.log("server running on " + port)
+    console.log("server running on " + port);
+});
+
+io.on('connection', (socket) => {
+    counter++;
+    console.log(counter + ' someone connected');
 });
