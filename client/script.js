f2cbd244dad4dbc2560010a7c7fe2dcd6278caf1
Peter <dhondt.peter@gmail.com>
2021-04-20 15:20:49 +0200

    add username to identify who sent the message

--- client/script.js
+++ client/script.js
@@ -4,14 +4,21 @@ let target = document.getElementById('output');
 
 document.getElementById('sendToAll').addEventListener('click', function () {
     let message = document.getElementById('message').value;
-    socket.emit('sendToAll', (message));
+    let user = document.getElementById('username').value;
+    socket.emit('sendToAll', {username: user, text: message});
+
+    // Clear the input field and put focus onto the field
+    message.value = '';
+    //message.focus();
 })
 
 document.getElementById('sendToMe').addEventListener('click', function () {
     let message = document.getElementById('message').value;
-    socket.emit('sendToMe', (message));
+    let user = document.getElementById('username').value;
+    socket.emit('sendToMe', {username: user, text: message});
 })
 
 socket.on('displayMessage', (message) => {
-    target.innerHTML += '<br>' + message;
+    console.log(message)
+    target.innerHTML += '<br>' + message.username + ' wrote: ' + message.text;
 })
--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ const server = http.createServer(app);
 
 const io = require('socket.io')(server);
 
+// import the formatMessage module we created to be able to format our messages with a username and a timestamp
+const formatMessage = require('../utils/messages');
+
 const port = 3000;
 
 let counter = 0;
@@ -17,13 +20,26 @@ server.listen(port, () => {
     console.log("server running on " + port);
 });
 
+// This runs when a client connects
 io.on('connection', (socket) => {
     counter++;
     console.log(counter + ' someone connected');
-    socket.on('sendToAll', (message) => {
-        io.emit('displayMessage', (message));
+
+    // Listen for a message to all connected clients
+    socket.on('sendToAll', (data) => {
+        io.emit('displayMessage', (data));
     });
-    socket.on('sendToMe', (message) => {
-        socket.emit('displayMessage', (message));
+
+    // Message only to the originating client
+    socket.on('sendToMe', (data) => {
+        socket.emit('displayMessage', (data));
+    });
+
+    // Message to all clients except the originating client
+    socket.broadcast.emit('displayMessage', 'A user has joined the room');
+
+    // Message when a client disconnects
+    socket.on('disconnect', () => {
+        io.emit('displayMessage', 'A user has left the room');
     });
 });
